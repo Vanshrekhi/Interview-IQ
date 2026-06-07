4529a6e92f5ee6d27bd6cd9581f60e5c0f1cb098
Vansh Rekhi <vanshrekhi05@gmail.com>
2026-06-08 01:57:55 +0530

    fixed auth import

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,24 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'ui': ['motion', 'react-icons'],
+          'api': ['axios'],
+        }
+      }
+    }
+  },
+  server: {
+    port: 5173,
+    host: true,
+  }
 })
