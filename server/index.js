0744cea01aab6b99f9d14ccc39317b3eeeb7a3f2
Vansh Rekhi <vanshrekhi05@gmail.com>
2026-07-04 22:22:49 +0530

    Fix chnag32s

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import interviewRouter from "./routes/interview.route.js"
 import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
+app.set("trust proxy", 1)
 app.use(cors({
     origin:"https://interview-iq-1-client.onrender.com",
     credentials:true
