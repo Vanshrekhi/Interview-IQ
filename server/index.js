2dca9090ef340115b33ab87728e63d441e846bb3
Vansh Rekhi <vanshrekhi05@gmail.com>
2026-07-04 21:01:47 +0530

    Fix chnages

--- server/index.js
+++ server/index.js
@@ -9,11 +9,16 @@ import userRouter from "./routes/user.route.js"
 import interviewRouter from "./routes/interview.route.js"
 import paymentRouter from "./routes/payment.route.js"
 
+const CLIENT_URL = process.env.CLIENT_URL || "https://interview-iq-1-client.onrender.com"
 const app = express()
+app.set("trust proxy", 1)
 app.use(cors({
-    origin:"https://interview-iq-1-client.onrender.com",
-    credentials:true
+    origin: CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
+app.options("*", cors({ origin: CLIENT_URL, credentials: true }))
 
 app.use(express.json())
 app.use(cookieParser())
